a49ddcb4310da68441d04167280a1e8f5be405cd
Ali Çalışkan <ali.caliskan1@outlook.com>
2024-05-26 17:52:33 +0300

    Add project files.

---
+++ HotelFinder.Entities/Hotel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HotelFinder.Entities
+{
+	public class Hotel
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
