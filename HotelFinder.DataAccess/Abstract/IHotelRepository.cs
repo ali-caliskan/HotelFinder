a57a82c5609a5dc327b2db7dfea625999b6b9081
Ali Çalışkan <ali.caliskan1@outlook.com>
2024-05-26 19:05:13 +0300

    Abstract klasörü eklendi. Interface tutulacağı kısım.

---
+++ HotelFinder.DataAccess/Abstract/IHotelRepository.cs
@@ -0,0 +1,26 @@
+﻿using HotelFinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.DataAccess.Abstract
+{
+	public  interface IHotelRepository
+	{
+		//Liste Türünden Hotelleri döndürücek.(Parametre Almıyacak)
+		List<Hotel> GetAllHotels();
+
+		//Hotel döndürücek.
+		Hotel GetHotelById(int id);
+
+		//Ekleme methodu.(Eklediğimiz hoteli geriye döndürücek.)
+		Hotel CreateHotel(Hotel hotel);
+
+		//update methodu.(Eklediğimiz hoteli geriye döndürücek)
+		Hotel UpdateHotel(Hotel hotel);
+
+		void DeleteHotel(int id);
+	}
+}
