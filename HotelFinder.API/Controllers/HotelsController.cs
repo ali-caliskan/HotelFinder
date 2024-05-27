287e6b7ae18f4dc6fcbbe337a99a52a21c46b4ae
Ali Çalışkan <ali.caliskan1@outlook.com>
2024-05-27 11:00:58 +0300

    API Controller oluşturuldu.

---
+++ HotelFinder.API/Controllers/HotelsController.cs
@@ -0,0 +1,34 @@
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.Business.Concrete;
+using HotelFinder.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace HotelFinder.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HotelsController : ControllerBase
+	{
+		private IHotelService _hotelservice;
+
+		public HotelsController()
+		{
+			_hotelservice = new HotelManager();
+		}
+
+		[HttpGet]
+		public List<Hotel> Get()
+		{
+			return _hotelservice.GetAllHotels();
+		}
+
+		[HttpGet("{id}")]
+		public Hotel Get(int id)
+		{
+			return _hotelservice.GetHotelById(id);
+		}
+
+	}
+}
